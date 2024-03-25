d21030ecf29e5604d9c7efadf60c8c7cf9541407
vishal-rajpoot <vishalrajpoot540@gmail.com>
2024-03-25 19:20:22 +0530

    bug: use context code commented

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,7 +4,7 @@ import { Context } from '../../context/Context';
 import './Main.css'
 
 const Main = () => {
-    const [onSent, recentPrompt, showResult, loading, resultData, setInput, input] = useContext(Context);
+    // const [onSent, recentPrompt, showResult, loading, resultData, setInput, input] = useContext(Context);
   return (
     <div className = 'main'>
         <div className = 'nav'>
@@ -36,11 +36,15 @@ const Main = () => {
             </div>
             <div className='main-bottom'>
                 <div className = 'search-box'>
-                    <input onChange={(e) => setInput(e.target.value)} value={input} type ='text' placeholder = 'Enter a prompt here'/>
+                    <input
+                    //  onChange={(e) => setInput(e.target.value)} value={input} 
+                     type ='text' placeholder = 'Enter a prompt here'/>
                     <div>
                         <img src = {assets.gallery_icon} alt =""/>
                         <img src = {assets.mic_icon} alt =""/>
-                        <img onClick ={() => onSent()} src = {assets.send_icon} alt =""/>
+                        <img 
+                        // onClick ={() => onSent()} 
+                        src = {assets.send_icon} alt =""/>
                     </div>
                 </div>
                 <p className='bottom-info'>
